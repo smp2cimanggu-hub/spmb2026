3f2fa3f9767928007fa49662c80dc8da829f6570
smp2cimanggu-hub <smp2cimanggu@gmail.com>
2026-05-07 14:15:25 +0700

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 // Service to interact with Google Apps Script Backend
 
 // To use the real backend, replace this URL with your deployed Google Apps Script Web App URL
-const GAS_WEB_APP_URL = ""; 
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzjUbfcIgVt4LtVvC7s8CZpSZw-ZAkpUvB_dQ4uMhxnTD550fFXJtjOSZ_OXWTx_wA1bw/exec"; 
 
 export interface FormField {
   id: string;
